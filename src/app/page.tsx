45406e5c9eb907e4331ecfbec8a82feef541bdc0
Maria Yuzba <yuzba.maria@gmail.com>
2025-06-09 13:35:35 +0100

    feat: add responsive header with mobile and desktop versions, delete unnecessary files (Home folder with its content), add variables to globals css to match Figma styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Header from "./components/Header"
+
 export default function Home() {
   return (
-    <main>
-      <h1>Help Chain</h1>
-    </main>
+    <>
+      <Header />
+    </>
+
   )
 }
