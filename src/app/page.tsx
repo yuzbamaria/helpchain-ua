e9c34cdff4d478c8a7eeb8103e6a2c2985771426
Yurii Petrovskyi <yurakarpaty@gmail.com>
2025-06-10 11:29:54 +0100

    fix: add WhatWeDo component into main page
    relates #18

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from './components/Hero';
 import HowItWorks from './components/HowItWorks';
+import WhatWeDo from './components/WhatWeDo';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <WhatWeDo />
       <HowItWorks />
     </main>
   );
