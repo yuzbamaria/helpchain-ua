f19f099e534d8dc135f4624705d007d37395a2ac
Maria Yuzba <yuzba.maria@gmail.com>
2025-06-12 21:25:47 +0100

    refactor: move components folder from app folder onto the same level with app folder, update paths in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Hero from './components/Hero';
-import HowItWorks from './components/HowItWorks';
-import WhatWeDo from './components/WhatWeDo';
-import Header from './components/Header';
-import Partners from "./components/Partners";
+import Hero from '../components/Hero';
+import HowItWorks from '../components/HowItWorks';
+import WhatWeDo from '../components/WhatWeDo';
+import Header from '../components/Header';
+import Partners from "../components/Partners";
 
 export default function Home() {
   return (
