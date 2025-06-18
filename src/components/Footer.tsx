153dc60613555960cc99ea4007e7606f0d0a5b7e
Maria Yuzba <yuzba.maria@gmail.com>
2025-06-18 14:40:30 +0100

    feat: add mobile design (from 375px up to 639px, add design for screen 1024px and up

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,7 +17,7 @@ const socialLinks = [
         icon: instagram
     }, 
     {
-        name: "xIcon",
+        name: "X",
         icon: xIcon
     },
     {
@@ -32,68 +32,203 @@ const socialLinks = [
 
 export default function Footer() {
     return (
-        <section className="flex justify-center text-white font-roboto">
-            <div className="flex flex-col gap-20 bg-primary-800 2xl:w-[1280px] p-10">
-                <div className="flex gap-20">
-                    <div className="flex flex-col gap-6">
-                        <div>{logoLight}</div>
-                        <p>Subscribe to our newsletter for the latest updates on features and releases.</p>
-                        <div>
-                            <form className="flex gap-4">
-                                <input 
-                                    type="text"
-                                    placeholder="Your Email Here"
-                                    className="w-[405px] rounded-sm p-3 border border-primary-400 placeholder-white"
-                                    required
-                                />
-                                <input 
-                                    type="submit" 
-                                    value="Join"
-                                    className="w-[79px] rounded-sm border border-primary-400 px-6 py-3" 
-                                />
-                            </form>
+        <section className="text-white font-roboto">
+             {/* Desktop Layout (xl and up) */}
+            <div className="hidden xl:flex justify-center">
+                <div className="flex flex-col gap-20 bg-primary-800 2xl:w-[1280px] p-10">
+                    <div className="flex gap-32">
+                        <div className="flex flex-col gap-6 2xl:w-[500px]">
+                            <div>{logoLight}</div>
+                            <p>Subscribe to our newsletter for the latest updates on features and releases.</p>
+                            <div>
+                                <form className="flex gap-4">
+                                    <input 
+                                        type="text"
+                                        placeholder="Your Email Here"
+                                        className="w-[405px] rounded-sm p-3 border border-primary-400 placeholder-white"
+                                        required
+                                    />
+                                    <input 
+                                        type="submit" 
+                                        value="Join"
+                                        className="w-[79px] rounded-sm border border-primary-400 px-6 py-3" 
+                                    />
+                                </form>
+                            </div>
+                            <p className="text-xs">By subscribing, you consent to our Privacy Policy and agree to receive updates.</p>
                         </div>
-                        <p className="text-xs">By subscribing, you consent to our Privacy Policy and agree to receive updates.</p>
+                        <div className="flex gap-20">
+                            <div className="flex flex-col gap-4">
+                                <p className="text-base font-semibold">Useful Links</p>
+                                <ul className="flex flex-col gap-3">
+                                    {usefulLinks.map((link, index) => (
+                                        <li key={index} className="font-normal text-sm py-2">{link}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className="flex flex-col gap-4">
+                                <p className="text-base font-semibold">Connect With Us</p>
+                                <ul className="flex flex-col gap-3">
+                                    {connectWithUs.map((link, index) => (
+                                        <li key={index} className="font-normal text-sm py-2">{link}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className="flex flex-col gap-4">
+                                <p className="text-base font-semibold">Follow Us</p>
+                                <ul className="flex flex-col gap-3">
+                                    {socialLinks.map((resource, index) => (
+                                        <li key={index} className="flex items-center font-normal text-sm py-2 gap-3">
+                                            <span>{resource.icon}</span>{resource.name}
+                                        </li>
+                                    ))}
+                                    
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                    <div className="flex gap-20 pr-10">
-                        <div className="flex flex-col gap-4">
-                            <p className="text-base font-semibold">Useful Links</p>
-                            <ul className="flex flex-col gap-3">
-                                {usefulLinks.map((link, index) => (
-                                    <li key={index} className="font-normal text-sm py-2">{link}</li>
-                                ))}
-                            </ul>
+                    <div className="flex justify-between border-t-[2px] border-primary-700 pt-8">
+                        <div>
+                            <p className="text-3.5 font-karla">© 2024 Relume. All rights reserved.</p>
                         </div>
-                        <div className="flex flex-col gap-4">
-                            <p className="text-base font-semibold">Connect With Us</p>
-                            <ul className="flex flex-col gap-3">
-                                {connectWithUs.map((link, index) => (
-                                    <li key={index} className="font-normal text-sm py-2">{link}</li>
-                                ))}
-                            </ul>
-                        </div>
-                        <div className="flex flex-col gap-4">
-                            <p className="text-base font-semibold">Follow Us</p>
-                            <ul className="flex flex-col gap-3">
-                                {socialLinks.map((resource, index) => (
-                                    <li key={index} className="flex items-center font-normal text-sm py-2 gap-3">
-                                        <span>{resource.icon}</span>{resource.name}
-                                    </li>
-                                ))}
-                                
-                            </ul>
-                        </div>
+                        <ul className="flex gap-6">
+                            <li>Privacy Policy</li>
+                            <li>Terms of Service</li>
+                            <li>Cookie Settings</li>
+                        </ul>
                     </div>
                 </div>
-                <div className="flex justify-between border-t-[2px] border-primary-700 pt-8">
-                    <div>
-                        <p className="text-3.5 font-karla">© 2024 Relume. All rights reserved.</p>
+            </div>
+
+            {/* lg:1024px and up */}
+            <div className="hidden xl:hidden lg:flex justify-center">
+                <div className="flex flex-col gap-12 bg-primary-800 w-[980px] p-10">
+                    <div className="flex gap-18">
+                        <div className="flex flex-col gap-6 w-[400px]">
+                            <div>{logoLight}</div>
+                            <p>Subscribe to our newsletter for the latest updates on features and releases.</p>
+                            <div>
+                                <form className="flex gap-4">
+                                    <input 
+                                        type="text"
+                                        placeholder="Your Email Here"
+                                        className="w-[305px] rounded-sm p-3 border border-primary-400 placeholder-white"
+                                        required
+                                    />
+                                    <input 
+                                        type="submit" 
+                                        value="Join"
+                                        className="w-[79px] rounded-sm border border-primary-400 px-6 py-3" 
+                                    />
+                                </form>
+                            </div>
+                            <p className="text-xs">By subscribing, you consent to our Privacy Policy and agree to receive updates.</p>
+                        </div>
+                        <div className="flex gap-11">
+                            <div className="flex flex-col gap-4">
+                                <p className="text-base font-semibold">Useful Links</p>
+                                <ul className="flex flex-col gap-3">
+                                    {usefulLinks.map((link, index) => (
+                                        <li key={index} className="font-normal text-sm py-2">{link}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className="flex flex-col gap-4">
+                                <p className="text-base font-semibold">Connect With Us</p>
+                                <ul className="flex flex-col gap-3">
+                                    {connectWithUs.map((link, index) => (
+                                        <li key={index} className="font-normal text-sm py-2">{link}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className="flex flex-col gap-4">
+                                <p className="text-base font-semibold">Follow Us</p>
+                                <ul className="flex flex-col gap-3">
+                                    {socialLinks.map((resource, index) => (
+                                        <li key={index} className="flex items-center font-normal text-sm py-2 gap-3">
+                                            <span>{resource.icon}</span>{resource.name}
+                                        </li>
+                                    ))}
+                                    
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                    <ul className="flex gap-6">
-                        <li>Privacy Policy</li>
-                        <li>Terms of Service</li>
-                        <li>Cookie Settings</li>
-                    </ul>
+                    <div className="flex justify-between border-t-[2px] border-primary-700 pt-8">
+                        <div>
+                            <p className="text-3.5 font-karla">© 2024 Relume. All rights reserved.</p>
+                        </div>
+                        <ul className="flex gap-6">
+                            <li>Privacy Policy</li>
+                            <li>Terms of Service</li>
+                            <li>Cookie Settings</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            {/* mobile 375px and up */}
+            <div className="lg:hidden flex justify-center">
+                <div className="flex flex-col gap-12 bg-primary-800 py-10 px-4">
+                    <div className="flex flex-col gap-12">
+                        <div className="flex flex-col gap-6 w-[340px]">
+                            <div>{logoLight}</div>
+                            <p>Subscribe to our newsletter for the latest updates on features and releases.</p>
+                            <div>
+                                <form className="flex flex-col gap-4">
+                                    <input 
+                                        type="text"
+                                        placeholder="Your Email Here"
+                                        className="w-[305px] rounded-sm p-3 border border-primary-400 placeholder-white"
+                                        required
+                                    />
+                                    <input 
+                                        type="submit" 
+                                        value="Join"
+                                        className="w-[79px] rounded-sm border border-primary-400 px-6 py-3" 
+                                    />
+                                </form>
+                            </div>
+                            <p className="text-xs">By subscribing, you consent to our Privacy Policy and agree to receive updates.</p>
+                        </div>
+                        <div className="flex gap-13">
+                            <div className="flex flex-col gap-4">
+                                <p className="text-base font-semibold">Useful Links</p>
+                                <ul className="flex flex-col gap-1 font-normal text-sm">
+                                    <li className="py-1">About Us</li>
+                                    <li className="py-1">Contact Us</li>
+                                    <li className="py-1">Careers</li>
+                                    <li className="py-1">Blog Posts</li>
+                                    <li className="py-1">Support Center</li>
+                                    <li className="py-1">Privacy Policy</li>
+                                    <li className="py-1">Terms of Service</li>
+                                    <li className="py-1">Cookie Settings</li>
+                                </ul>
+                            </div>
+                            <div className="flex flex-col gap-4">
+                                <p className="text-base font-semibold">Connect With Us</p>
+                                <ul className="flex flex-col gap-1">
+                                    {connectWithUs.map((link, index) => (
+                                        <li key={index} className="font-normal text-sm py-1">{link}</li>
+                                    ))}
+                                </ul>
+                                <ul className="flex gap-6">
+                        </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center gap-4 border-t-[2px] border-primary-700 pt-8">
+                        <div>
+                            <p className="text-3.5 font-karla">© 2024 Relume. All rights reserved.</p>
+                        </div>
+                        <ul className="flex gap-10">
+                            {socialLinks.map((resource, index) => (
+                                <li key={index} className="font-normal text-sm py-2 gap-3">
+                                    <span>{resource.icon}</span>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             </div>
         </section>
