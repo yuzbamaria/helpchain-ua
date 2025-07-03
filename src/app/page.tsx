b330ddf61e97267a630fc6a7ad6a00c62af2e16e
Yurii Petrovskyi <yurakarpaty@gmail.com>
2025-07-03 15:43:45 +0100

    refactor: update header, page layout and styling (relates #33)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import Hero from '../components/Hero';
 import HowItWorks from '../components/HowItWorks';
 import WhatWeDo from '../components/WhatWeDo';
-import Header from '../components/Header';
+// import Header from '../components/Header';
 import Partners from "../components/Partners";
 import Action from '@/components/Action';
 
 export default function Home() {
   return (
     <main>
-      <Header />
       <Hero />
       <WhatWeDo />
       <HowItWorks />
