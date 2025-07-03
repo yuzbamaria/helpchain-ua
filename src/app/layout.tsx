58266436545402ff34ff9d9a4d2b201e308011a9
Yurii Petrovskyi <yurakarpaty@gmail.com>
2025-07-03 15:41:58 +0100

    feat(wrapper): wrap app with SessionProvider and layout updates (relates #33)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,23 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "@/components/Header";
+import AuthWrapper from "@/components/AuthWrapper";
 
 export const metadata: Metadata = {
   title: "Help Chain",
   description: "Job search app",
 };
 
-export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode;}>) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
+      <body className="h-full">
+        <AuthWrapper>
+          <Header />
+          {children}
+        </AuthWrapper>
       </body>
     </html>
   );
