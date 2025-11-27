f4f3ebfc5cc5fb45e3759d2161875eb0e84fa50b
Maria Yuzba <yuzba.maria@gmail.com>
2025-11-27 22:15:15 +0000

    fix: update the size of the email icon to not break responsiveness

--- src/icons/Email.tsx
+++ src/icons/Email.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const Email: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg
-    width="375"
-    height="163"
+    width="100%"
+    height="auto"
     viewBox="0 0 375 163"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    {...props}
   >
     <path
       d="M235.042 33.958H139.958C132.456 33.958 126.375 40.0395 126.375 47.5413V115.458C126.375 122.96 132.456 129.041 139.958 129.041H235.042C242.544 129.041 248.625 122.96 248.625 115.458V47.5413C248.625 40.0395 242.544 33.958 235.042 33.958Z"
